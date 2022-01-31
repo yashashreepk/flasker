a63f645fc373569d3aada20e403d3af15b37a841
yashashreepk <yashashreepk@gmail.com>
2022-01-31 12:26:23 +0530

    tweaked 500.html

--- hello.py
+++ hello.py
@@ -37,6 +37,7 @@ def user(name):
 def page_not_found(e):
     return render_template("404.html"),404
 
+# internal server error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"),500
